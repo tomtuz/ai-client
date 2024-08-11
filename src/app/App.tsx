0c80fb90e2692d511864758bba12913de60d9daa
tomtuz <25122240+tomtuz@users.noreply.github.com>
2024-08-11 04:05:04 +0300

    feat: initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="w-full bg-red-300">Hello Wolrld!</h1>
+      <div className="w-full font-bold">Chat init</div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { URL, fileURLToPath } from 'node:url'
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
