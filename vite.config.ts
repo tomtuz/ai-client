2d670b232e4e477dfe6c301776430c4f3fb5188a
Tomas Tuzikas <encona.forte@gmail.com>
2024-08-12 18:44:17 +0300

    fix(tsconfig): revert to 3 file config system

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
-import { URL, fileURLToPath } from 'node:url'
+// import { URL, fileURLToPath } from 'node:url'
 import react from "@vitejs/plugin-react-swc";
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@twc': fileURLToPath(new URL('./src/components/tailwind', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components/default', import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
+      '@twc': path.resolve(__dirname, './src/components/tailwind'),
+      '@components': path.resolve(__dirname, './src/components/default'),
+      // '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // '@twc': fileURLToPath(new URL('./src/components/tailwind', import.meta.url)),
+      // '@components': fileURLToPath(new URL('./src/components/default', import.meta.url)),
     },
   },
   envPrefix: "EXPOSE_"
